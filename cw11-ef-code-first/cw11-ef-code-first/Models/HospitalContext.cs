326ce5978641c7b1b3720b1e0711017134a85739
Ewa Zielińska <s16475@pjwstk.edu.pl>
2020-06-05 21:00:06 +0200

    migracje1

---
+++ cw11-ef-code-first/cw11-ef-code-first/Models/HospitalContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace cw11_ef_code_first.Models
+{
+    public class HospitalContext : DbContext
+    {
+
+        public DbSet<Doctor> Doctors { get; set; }
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<Prescription> Prescriptions { get; set; }
+
+        public HospitalContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+    }
+}
---
+++ cw11-ef-code-first/cw11-ef-code-first/Models/Prescription.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace cw11_ef_code_first.Models
+{
+    public class Prescription
+    {
+        [Key]
+        public int IdPrescription { get; set; }
+        [Required]
+        public DateTime Date { get; set; }
+        [Required]
+        public DateTime DueDate { get; set; }
+        [ForeignKey("Patient")]
+        public int IdPatient { get; set; }
+        public Patient Patient { get; set; }
+        [ForeignKey("Doctor")]
+        public int IdDoctor { get; set; }
+        public Doctor Doctor { get; set; }
+
+
+    }
+}
